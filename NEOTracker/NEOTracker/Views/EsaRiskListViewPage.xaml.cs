d36dfb098eb86411eba979318e0b186b7a15b4e0
John Bristowe <john@bristowe.com>
2017-04-21 18:32:54 +1000

    Integrated ESA risk list and chart

---
+++ NEOTracker/NEOTracker/Views/EsaRiskListViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NEOTracker.Data;
+using System.Collections.Generic;
+using Telerik.UI.Xaml.Controls.Grid;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace NEOTracker.Views
+{
+    public sealed partial class EsaRiskListViewPage : Page
+    {
+        public EsaRiskListViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var esaData = await EsaDataManager.GetEsaDataAsync();
+            DataContext = esaData;
+        }
+    }
+}
